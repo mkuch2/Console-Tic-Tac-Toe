8f12cf544ca9d556809c9986608911956fb83908
mkuch <kuchimpos@g.ucla.edu>
2024-10-17 15:00:39 -0700

    Add getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello world")
+console.log("Hello world");
+
+function getComputerChoice()
+{
+  num = Math.trunc((Math.random() * 3));
+  switch(num){
+    case 0:
+      console.log("rock");
+      return "rock";
+    case 1:
+      console.log("paper");
+      return "paper";
+    case 2:
+      console.log("scissors");
+      return "scissors";
+  }
+}
+
+
